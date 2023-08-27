1fe1c9e14b17a58eb4e170db3d3128f28f7af10d
Deric Mathenge <deric.mathenge@savannahinformatics.com>
2023-08-27 22:32:30 +0300

    Feature: user authentication register login password

--- real_estate/settings/development.py
+++ real_estate/settings/development.py
@@ -1,5 +1,15 @@
 from .base import *  # noqa: F403
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = env("EMAIL_HOST")  # noqa
+EMAIL_USE_TLS = True  # noqa
+EMAIL_PORT = env("EMAIL_PORT")  # noqa
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")  # noqa
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")  # noqa
+DEFAULT_FROM_EMAIL = 'mianoderic@gmail.com'
+DOMAIN = env('DOMAIN')  # noqa
+SITE_NAME = "Real Estate"
+
 DATABASES = {
     "default": {
         "ENGINE": env("POSTGRES_ENGINE"),  # noqa
