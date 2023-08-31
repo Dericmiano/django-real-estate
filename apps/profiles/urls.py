b657e22565bc726f6e2c3bc55ee0e520b8e3a854
Deric Mathenge <deric.mathenge@savannahinformatics.com>
2023-08-31 04:29:25 +0300

    Register login update user profile

---
+++ apps/profiles/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import (AgentListApiView, GetProfileApiView, TopAgentsListAPIView,
+                    UpdateProfileApiView)
+
+
+urlpatterns = [
+    path('me/', GetProfileApiView.as_view(), name="get_profile"),
+    path('update/<str:username>/', UpdateProfileApiView.as_view(), name="update_profile"),
+    path('agents/all/', AgentListApiView.as_view(), name="all-agents"),
+    path('top-agents/all/', TopAgentsListAPIView.as_view(), name="top-agents"),
+]
