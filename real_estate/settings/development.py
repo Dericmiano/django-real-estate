38ec0bc6139d4d50dfef11aab953cf7e0b79c89a
Deric Mathenge <deric.mathenge@savannahinformatics.com>
2023-09-23 18:35:43 +0300

    Setting up celery and flower sent enquiries through it

--- real_estate/settings/development.py
+++ real_estate/settings/development.py
@@ -1,6 +1,7 @@
 from .base import *  # noqa: F403
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
 EMAIL_HOST = env("EMAIL_HOST")  # noqa
 EMAIL_USE_TLS = True  # noqa
 EMAIL_PORT = env("EMAIL_PORT")  # noqa
@@ -20,3 +21,7 @@ DATABASES = {
         "PORT": env("PG_PORT"),  # noqa
     }
 }
+
+CELERY_BROKER_URL = env("CELERY_BROKER") # noqa
+CELERY_RESULT_BACKEND = env("CELERY_BACKEND") # noqa
+CELERY_TIMEZONE = "Africa/Nairobi"
