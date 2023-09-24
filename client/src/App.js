d8014fad03fa952447d47db8e34e8cead29f452b
Deric Mathenge <deric.mathenge@savannahinformatics.com>
2023-09-24 14:24:29 +0300

    Set up nginx and react with docker

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from "react"
+
+function App() {
+  return (
+    <div>
+<h1>Real Estate Nigga</h1>
+<p>
+  The real test starts here
+</p>
+    </div>
+  );
+}
+
+export default App;
