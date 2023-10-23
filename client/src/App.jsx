2f09adf8a7d82a9fdeb6c4d68f5f73643d059e03
Deric Mathenge <deric.mathenge@savannahinformatics.com>
2023-10-23 21:56:44 +0300

    chore: added not found page and set up redux

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import {ToastContainer} from 'react-toastify'
+import "react-toastify/dist/ReactToastify.css"
 import Header from './components/Header';
 import Footer from './components/Footer';
 import {Homepage}  from './pages/Homepage';
 import { ProprtiesPage } from './pages/ProprtiesPage';
+import { NotFound } from './components/NotFound';
 const  App = () => {
   return (
     <>
@@ -12,15 +14,14 @@ const  App = () => {
       <Header/>
       <main className='py-3'>
         <Routes>
-          <Route path='/' element={<Homepage/>}></Route>
-        </Routes>
-        <Routes>
+          <Route path='/' element={<Homepage/>}/>
           <Route path='/properties'element={<ProprtiesPage/>}></Route>
+          <Route path='*' element={<NotFound/>} />
         </Routes>
+        <ToastContainer theme='dark'/>
       </main>
       <Footer/>
     </Router>
-    <ToastContainer/>
     </>
   );
 }
