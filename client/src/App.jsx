56737bbf3e6a4aaa349d88b790e1d4379d246c55
Deric Mathenge <deric.mathenge@savannahinformatics.com>
2023-10-23 03:56:58 +0300

    chore: setup basic home setup and properties change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import {ToastContainer} from 'react-toastify'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import {Homepage}  from './pages/Homepage';
+import { ProprtiesPage } from './pages/ProprtiesPage';
 const  App = () => {
   return (
-    <div className="App">
-     <h1>Welcome at real estate</h1>
-    </div>
+    <>
+    <Router>
+      <Header/>
+      <main className='py-3'>
+        <Routes>
+          <Route path='/' element={<Homepage/>}></Route>
+        </Routes>
+        <Routes>
+          <Route path='/properties'element={<ProprtiesPage/>}></Route>
+        </Routes>
+      </main>
+      <Footer/>
+    </Router>
+    <ToastContainer/>
+    </>
   );
 }
 
---
+++ client/src/pages/ProprtiesPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Col, Container,Row} from 'react-bootstrap'
+
+export const ProprtiesPage = () => {
+  return (
+    <>
+    <Container>
+        <Row>
+            <Col className='mg-top'>
+                <h1>Properties Will be displayed heir</h1>
+            </Col>
+        </Row>
+    </Container></>
+  )
+}
