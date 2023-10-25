98a7d4dda5dc3ea5359d290cfccd201c1a36283b
Deric Mathenge <deric.mathenge@savannahinformatics.com>
2023-10-25 04:18:12 +0300

    chore: add propery component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import {ToastContainer} from 'react-toastify'
-import "react-toastify/dist/ReactToastify.css"
+import "react-toastify/dist/ReactToastify.css";
 import Header from './components/Header';
 import Footer from './components/Footer';
 import {Homepage}  from './pages/Homepage';
--- client/src/pages/ProprtiesPage.jsx
+++ client/src/pages/ProprtiesPage.jsx
@@ -3,13 +3,17 @@ import {Col, Container,Row} from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Spinner from '../components/Spinner'
 import {getProperties, reset} from '../features/properties/propertySlice'
-
+import {toast} from 'react-toastify'
+import Property from '../components/Property'
 export const ProprtiesPage = () => {
-    const {properties, isLoading, isSuccess} = useSelector((state)=> state.properties)
+    const {properties, isLoading, isError, message} = useSelector((state)=> state.properties)
     const dispatch = useDispatch()
     useEffect(()=>{
+        if (isError) {
+            toast.error(message, {icon:"😭"})
+        }
         dispatch(getProperties())
-    },[dispatch])
+    },[dispatch,isError,message])
 
     if (isLoading){
         return <Spinner/>
@@ -18,10 +22,22 @@ export const ProprtiesPage = () => {
     <>
     <Container>
         <Row>
-            <Col className='mg-top'>
-                <h1>Properties Will be displayed heir</h1>
+            <Col className='mg-top text-centre'>
+                <h1>Our catalog of properties</h1>
+                <hr className='hr-text' />
             </Col>
         </Row>
+        {
+            <>
+            <Row className='mt-3 mr-5'>
+                {properties.map((property)=>(
+                    <Col key={property.id} sm={12} md={6} lg={4} xl={3} className='mr-5'>
+                        <Property property={property} />
+                    </Col>
+                ))}
+            </Row>
+            </>
+        }
     </Container>
     </>
   )
